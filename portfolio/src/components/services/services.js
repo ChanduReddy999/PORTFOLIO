ce3477bae5dcc6b5adcfb672d9d75522797e92f3
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2024-04-05 22:37:14 +0530

    added serveices files

--- portfolio/src/components/services/services.js
+++ portfolio/src/components/services/services.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import './services.css'
+import checkListIcon from '../../Images/checkListIcon.png'
 
 const Services = () => {
   return (
     <>
-        <div id='services'>
-            <h1>Services</h1>
-            <p>As a fresher in web development, I specialize in creating and integrating web services to provide seamless user experiences</p>
-            <div>
-                <div>
-                    <img src='#' />
+        <div id='services' className='servicesMain'>
+            <h1 className='servicesH1'>Services</h1>
+            <p className='servicesP'>As a fresher in web development, I specialize in creating and integrating web services to provide seamless user experiences</p>
+            <div className='servicesSubDiv'>
+                <div className='servicesSubDivImgDiv1'>
+                    <img src={checkListIcon} className='servicesSubDivImg' />
                 </div>
-                <div>
-                    <h2>Web Development</h2>
-                    <p>Web developers create and maintain websites. They are also responsible for the site's technical aspects, such as its performance and capacity, which are measures of a website's speed and how much traffic the site can handle. In addition, web developers may create content for the site.</p>
+                <div className='servicesSubDivDiv2'>
+                    <h2 className='servicesSubDivDiv2H2'>Web Development</h2>
+                    <p className='servicesSubDivDiv2P'>Web developers create and maintain websites. They are also responsible for the site's technical aspects, such as its performance and capacity, which are measures of a website's speed and how much traffic the site can handle. In addition, web developers may create content for the site.</p>
                 </div>
             </div>
         </div>
