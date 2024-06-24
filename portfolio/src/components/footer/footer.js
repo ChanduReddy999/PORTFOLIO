21a282fa7c2fca86610d8de4c0dbb5a6e55399ec
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2024-06-24 23:22:41 +0530

    added css for mobile view

--- portfolio/src/components/footer/footer.js
+++ portfolio/src/components/footer/footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <>
       <div className='footerDiv'>
-        <p className='footerP1'>&#169;{PresentYear} All Rights Reserved</p>
+        <p className='footerP1'>&#169;{PresentYear} | All Rights Reserved</p>
         <p className='footerP2'>Designed by <span className='footerP2Span'>Chandu Reddy Vadala</span></p>
       </div>
     </>
